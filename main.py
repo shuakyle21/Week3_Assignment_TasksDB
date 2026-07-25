d38cc4af3a987bbe8433bbb5577f5eaf15b85fb3
Joshua Klyne Pudadera <nenitarmo@gmail.com>
2026-07-25 19:43:45 +0800

    Stage 1: database read endpoints
    Refactor task endpoints to use database and use dependency injection for database connection using the DRY principle

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
+from asyncio import tasks
+from fastapi import Depends, FastAPI, HTTPException
 import sqlite3
 
-from fastapi import FastAPI, HTTPException
 import db
+
 
 app = FastAPI()
 
-db_name = "tasks.db"
-conn = sqlite3.connect(db_name)
-cursor = conn.cursor()
+def get_db():
+    conn = sqlite3.connect(db.db_name)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+    return conn
 
 @app.get("/")
-async def root():
+def root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
 
 @app.get("/health")
-async def health():
+def health():
     return { "status": "ok" }
 
 @app.get("/tasks")
-async def get_tasks():
+def get_tasks(conn: sqlite3.Connection = Depends(get_db)):
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM tasks")
     tasks = cursor.fetchall()
-    return { task[0]: {"id": task[0], "title": task[1], "done": bool(task[2])} for task in tasks }
+    
+    return {"tasks": [{"id": task[0], "title": task[1], "done": bool(task[2])} for task in tasks]}
 
 @app.get("/tasks/{id}")
-def get_task(id: int):
-    for task in db.count_tasks():
-        if task["id"] == id:
-            return task
-        
-    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+def get_task(id: int, conn: sqlite3.Connection = Depends(get_db)):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE id = ?", (id,))
+    task = cursor.fetchone()
+
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task {id} not found, error code: {404}")
+
+    return {"id": task[0], "title": task[1], "done": bool(task[2])}
+
 
 @app.post("/tasks")
 def create_task(task: dict):
